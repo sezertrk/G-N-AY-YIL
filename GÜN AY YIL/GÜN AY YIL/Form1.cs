65336613d7a352d00623bc4a6bad7bd047bc446c
sezertrk <43032011+sezertrk@users.noreply.github.com>
2018-09-06 15:33:49 +0300

    Add files via upload

---
+++ GÜN AY YIL/GÜN AY YIL/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GÜN_AY_YIL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gun = int.Parse(textBox1.Text);
+            int ay = int.Parse(textBox2.Text);
+            int yil1 = int.Parse(textBox3.Text)/1000;
+            int yil2 = (int.Parse(textBox3.Text) - (yil1 * 1000)) / 100;
+            int yil3 = (int.Parse(textBox3.Text) - (yil1 * 1000) + (yil2 * 100)) / 10;
+            int yil4 = (int.Parse(textBox3.Text) - (yil1 * 1000) + (yil2 * 100) + (yil3 * 10));
+            int olyil = 0;
+            olyil = (ay + gun);
+
+          
+        }
+    }
+}
